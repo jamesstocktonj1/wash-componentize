0349bee07e785948ba1912f6bcdca9a0dcaf7c05
jamesstocktonj1 <james.stocktonj@gmail.com>
2026-02-15 18:40:48 +0000

    feat: add request handler

--- basic/gen/export_wasi_http_incoming_handler/handler.go
+++ basic/gen/export_wasi_http_incoming_handler/handler.go
@@ -3,14 +3,24 @@ package export_wasi_http_incoming_handler
 import (
 	httptypes "componentize-basic/gen/wasi_http_types"
 	"net/http"
+
+	wittypes "github.com/bytecodealliance/wit-bindgen/wit_types"
 )
 
 var handler http.HandlerFunc
 
 // Handle the specified `Request`, returning a `Response`
 func Handle(request *httptypes.IncomingRequest, responseOut *httptypes.ResponseOutparam) {
-	req := &http.Request{}
+	req, err := newRequest(request)
+	if err != nil {
+		Err := httptypes.MakeErrorCodeInternalError(wittypes.Some(err.Error()))
+		result := wittypes.Err[*httptypes.OutgoingResponse](Err)
+		httptypes.ResponseOutparamSet(responseOut, result)
+
+		return
+	}
 	res := newResponseWriter(responseOut)
+
 	handler(res, req)
 }
 
--- basic/gen/export_wasi_http_incoming_handler/request.go
+++ basic/gen/export_wasi_http_incoming_handler/request.go
@@ -2,9 +2,154 @@ package export_wasi_http_incoming_handler
 
 import (
 	httptypes "componentize-basic/gen/wasi_http_types"
+	streams "componentize-basic/gen/wasi_io_streams"
+	"errors"
+	"fmt"
+	"io"
 	"net/http"
+	"runtime"
+	"sync"
 )
 
 func newRequest(request *httptypes.IncomingRequest) (*http.Request, error) {
-	return nil, nil
+	method := parseMethod(request.Method())
+	if method == "" {
+		return nil, errors.New("unknown method type")
+	}
+
+	authority := "localhost"
+	if request.Authority().IsSome() {
+		authority = request.Authority().Some()
+	}
+
+	path := "/"
+	if request.PathWithQuery().IsSome() {
+		path = request.PathWithQuery().Some()
+	}
+
+	body, err := newRequestBody(request)
+	if err != nil {
+		return nil, err
+	}
+
+	url := fmt.Sprintf("http://%s%s", authority, path)
+	req, err := http.NewRequest(method, url, body)
+	if err != nil {
+		return nil, err
+	}
+
+	// TODO: copy headers
+	headers := request.Headers()
+	defer headers.Drop()
+
+	req.Host = authority
+	req.URL.Host = authority
+	req.RequestURI = path
+
+	return req, nil
+}
+
+func parseMethod(method httptypes.Method) string {
+	switch method.Tag() {
+	case httptypes.MethodGet:
+		return http.MethodGet
+	case httptypes.MethodHead:
+		return http.MethodHead
+	case httptypes.MethodPost:
+		return http.MethodPost
+	case httptypes.MethodPut:
+		return http.MethodPut
+	case httptypes.MethodDelete:
+		return http.MethodDelete
+	case httptypes.MethodConnect:
+		return http.MethodConnect
+	case httptypes.MethodOptions:
+		return http.MethodOptions
+	case httptypes.MethodTrace:
+		return http.MethodTrace
+	case httptypes.MethodPatch:
+		return http.MethodPatch
+	case httptypes.MethodOther:
+		method.Other()
+		return method.Other()
+	default:
+		return ""
+	}
+}
+
+func newRequestBody(request *httptypes.IncomingRequest) (io.ReadCloser, error) {
+	consumeRes := request.Consume()
+	if consumeRes.IsErr() {
+		return nil, fmt.Errorf("failed to consume incoming request: %+v", consumeRes.Err())
+	}
+	body := consumeRes.Ok()
+
+	streamRes := body.Stream()
+	if streamRes.IsErr() {
+		return nil, fmt.Errorf("failed to open request body stream: %+v", streamRes.Err())
+	}
+	stream := streamRes.Ok()
+
+	return &requestBody{
+		body:   body,
+		stream: stream,
+	}, nil
+}
+
+type requestBody struct {
+	body   *httptypes.IncomingBody
+	stream *streams.InputStream
+
+	flushOnce sync.Once
+}
+
+var _ io.ReadCloser = (*requestBody)(nil)
+
+func (r *requestBody) Read(p []byte) (int, error) {
+	pollable := r.stream.Subscribe()
+	for !pollable.Ready() {
+		runtime.Gosched()
+	}
+	defer pollable.Drop()
+
+	readRes := r.stream.Read(uint64(len(p)))
+	if readRes.IsErr() {
+		switch readRes.Err().Tag() {
+		case streams.StreamErrorClosed:
+			r.Close()
+			return 0, io.EOF
+		case streams.StreamErrorLastOperationFailed:
+			return 0, errors.New(readRes.Err().LastOperationFailed().ToDebugString())
+		default:
+			return 0, fmt.Errorf("unknown error reading from stream: %+v", readRes.Err())
+		}
+	}
+
+	data := readRes.Ok()
+	copy(p, data)
+	return len(data), nil
+}
+
+func (r *requestBody) Close() error {
+	r.flushOnce.Do(r.flush)
+
+	if r.stream != nil {
+		r.stream.Drop()
+		r.stream = nil
+	}
+
+	if r.body != nil {
+		r.body.Drop()
+		r.body = nil
+	}
+
+	return nil
+}
+
+func (r *requestBody) flush() {
+	futureTrailers := httptypes.IncomingBodyFinish(r.body)
+	defer futureTrailers.Drop()
+
+	futureTrailers.Get()
+	r.body = nil
 }
